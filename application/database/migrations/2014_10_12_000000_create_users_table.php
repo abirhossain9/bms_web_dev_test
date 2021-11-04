bbb6ac2d2a68a0d8a6810d4b34a57428665b1d49
Md Abir Hossain <abir.hossain04@northsouth.edu>
2021-11-05 00:17:17 +0600

    model and database updated

--- application/database/migrations/2014_10_12_000000_create_users_table.php
+++ application/database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,8 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('role')->default(2)->comment('1 for admin, 2 for user');
+            $table->string('admin_verification')->default(2)->comment('1 = verified, 2 = not verified');
             $table->rememberToken();
             $table->timestamps();
         });
