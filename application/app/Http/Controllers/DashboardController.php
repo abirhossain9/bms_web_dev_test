659efddb359cd81e27e9ffd1322ccbf40c624186
Md Abir Hossain <abir.hossain04@northsouth.edu>
2021-11-05 18:26:29 +0600

    Controller updated

--- application/app/Http/Controllers/DashboardController.php
+++ application/app/Http/Controllers/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
@@ -13,62 +14,19 @@ class DashboardController extends Controller
      */
     public function index()
     {
-        //
+        $user = 2;
+        $not_verified = 2;
+        $users = User::Orderby('id','asc')->WHERE('role',$user)->WHERE('admin_verification',$not_verified)->get();
+        return view('dashboard',compact('users'));
+
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
-        //
+        $user = User::find($id);
+        $user->admin_verification = 1;
+        $user->save();
+        return redirect()->route('dashboard')->with('user Verified Successfully');
     }
 
     /**
